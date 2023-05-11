fc3e7a482552974687b8b6656ff8c68955e427ea
Matias Camiletti <matias.camiletti@gmail.com>
2023-05-11 17:53:28 -0300

    Install dotenv library.

--- lib/src/http/tots_http.dart
+++ lib/src/http/tots_http.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class TotsHttp {
   static final _dio = Dio();
 
   static void configure() {
     // Configuraciones generales
-    _dio.options.baseUrl = 'https://api.example.com';
+    _dio.options.baseUrl = dotenv.env['API_BASE_URL']!;
     //_dio.options.connectTimeout = 5000; // 5 segundos
     //_dio.options.receiveTimeout = 5000; // 5 segundos
 
